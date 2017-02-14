e047e4e74d2af713a8c08bfe5b426a3a6dc64658
sbecot <simon.becot@orange.com>
2017-02-14 11:49:35 +0100

    Update configapp.js

--- config/configapp.js
+++ config/configapp.js
@@ -7,8 +7,7 @@ module.exports = {
 	'globlaRegistryUrl' : 'http://greg.rethink3.orange-labs.fr',
 	//'globlaRegistryUrl' : 'http://130.149.22.133', 
 	//'globlaRegistryPort' : '5002',
-	//'proxy' :'',
-	'proxy' : 'http://proxy.rd.francetelecom.fr:3128/',
+	'proxy' :'',
 	'domainRegistryUrl':'https://acor-webrtc.rethink2.orange-labs.fr/registry/',
 	'webRTCUrl':'https://acor-webrtc.rethink2.orange-labs.fr/'
 
