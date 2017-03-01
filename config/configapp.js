199b28b1c6152a10f7e953bad8aa58346bbba1dd
AlexTouze <alexandre.touze@orange.com>
2017-03-01 10:53:45 +0100

    Upadte ConfigFile

--- config/configapp.js
+++ config/configapp.js
@@ -2,12 +2,12 @@
 module.exports = {
     'url': 'localhost:27017', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     'contactDB': 'localhost:27017/contactsDB',
-    'globlaRegistryUrl': 'http://161.106.2.20:5002',
-    //'globlaRegistryUrl': 'http://greg.rethink3.orange-labs.fr',
+    //'globlaRegistryUrl' : 'http://161.106.2.20:5002',
+    'globlaRegistryUrl': 'http://greg.rethink3.orange-labs.fr',
     //'globlaRegistryUrl' : 'http://130.149.22.133', 
     //'globlaRegistryPort' : '5002',
-    'proxy': 'http://proxy.rd.francetelecom.fr:3128/',
+    'proxy': '',
     'domainRegistryUrl': 'https://acor-webrtc.rethink2.orange-labs.fr/registry/',
-    'webRTCUrl': 'https://acor-webrtc.rethink2.orange-labs.fr/',
+    'webRTCUrl': 'https://acor-webrtc.rethink2.orange-labs.fr/'
 
 };
