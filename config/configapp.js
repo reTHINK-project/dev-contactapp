7a106a12fb80b054fa2042876208328506564bbf
AlexTouze <alexandre.touze@orange.com>
2017-03-07 14:47:31 +0100

    Correction Style

--- config/configapp.js
+++ config/configapp.js
@@ -1,12 +1,12 @@
 // config/database.js
 module.exports = {
-    'url': 'mongoContact:27017', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'contactDB': 'mongoContact:27017/contactsDB',
+    'url': 'localhost:27017', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'contactDB': 'localhost:27017/contactsDB',
     //'globlaRegistryUrl' : 'http://161.106.2.20:5002',
     'globlaRegistryUrl': 'https://greg.rethink3.orange-labs.fr',
     //'globlaRegistryUrl' : 'http://130.149.22.133', 
     //'globlaRegistryPort' : '5002',
-    'proxy': '',
+    'proxy': 'http://proxy.rd.francetelecom.fr:3128/',
     'domainRegistryUrl': 'https://acor-webrtc.rethink2.orange-labs.fr/registry/',
     'webRTCUrl': 'https://acor-webrtc.rethink2.orange-labs.fr/'
 
